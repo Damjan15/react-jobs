01d78a4c2899648fb64ad52b09c47a7e44bf8bc7
Damjan15 <stamenovdamjan5@gmail.com>
2025-04-08 22:46:01 +0200

    feat: not found page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import Jobs from "./pages/Jobs";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Home />} />
       <Route path="/jobs" element={<Jobs />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
